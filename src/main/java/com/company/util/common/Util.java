f63fb89ecf7d0f4650b0f011e6c8687231590096
khomyakeugene <khomyak_eugene@yahoo.com>
2016-09-06 13:21:45 +0300

    Restaurant Project

--- src/main/java/com/company/util/common/Util.java
+++ src/main/java/com/company/util/common/Util.java
@@ -8,11 +8,9 @@ import java.util.Date;
  * Created by Yevhen on 06.09.2016.
  */
 public class Util {
-
-    public static Date getDateOnly(Timestamp timestamp) {
-        Date date = new Date(timestamp.getTime());
-
+    public static Date getDateOnly(Date date) {
         Calendar calendar = Calendar.getInstance();
+
         calendar.setTime(date);
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
@@ -20,5 +18,9 @@ public class Util {
         calendar.set(Calendar.MILLISECOND, 0);
 
         return calendar.getTime();
+    }
+
+    public static Date getDateOnly(Timestamp timestamp) {
+        return getDateOnly(new Date(timestamp.getTime()));
     }
 }
