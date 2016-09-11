54dae04a40542454723bebd251aec2438bb68c98
khomyakeugene <khomyak_eugene@yahoo.com>
2016-09-11 15:37:39 +0300

    Restaurant Project

--- src/main/java/com/company/restaurant/model/CookedCourse.java
+++ src/main/java/com/company/restaurant/model/CookedCourse.java
@@ -1,23 +1,24 @@
 package com.company.restaurant.model;
 
+import com.company.restaurant.model.common.SimpleObject;
+
 import java.sql.Timestamp;
 
 /**
  * Created by Yevhen on 14.06.2016.
  */
-public class CookedCourse {
-    private int cookedCourseId;
+public class CookedCourse extends SimpleObject {
     private Timestamp cookDatetime;
     private Float weight;
     private Course course = new Course();
     private Employee employee = new Employee();
 
     public int getCookedCourseId() {
-        return cookedCourseId;
+        return getId();
     }
 
     public void setCookedCourseId(int cookedCourseId) {
-        this.cookedCourseId = cookedCourseId;
+        setId(cookedCourseId);
     }
 
     public Timestamp getCookDatetime() {
@@ -56,23 +57,21 @@ public class CookedCourse {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof CookedCourse)) return false;
+        if (!super.equals(o)) return false;
 
         CookedCourse that = (CookedCourse) o;
 
-        return cookedCourseId == that.cookedCourseId && (cookDatetime != null ?
-                cookDatetime.equals(that.cookDatetime) :
-                that.cookDatetime == null && (weight != null ?
-                        weight.equals(that.weight) :
-                        that.weight == null && (course != null ?
-                                course.equals(that.course) :
-                                that.course == null && (employee != null ?
-                                        employee.equals(that.employee) : that.employee == null))));
+        return cookDatetime != null ? cookDatetime.equals(that.cookDatetime) :
+                that.cookDatetime == null && (weight != null ? weight.equals(that.weight) :
+                        that.weight == null && (course != null ? course.equals(that.course) :
+                                that.course == null && (employee != null ? employee.equals(that.employee) :
+                                        that.employee == null)));
 
     }
 
     @Override
     public int hashCode() {
-        int result = cookedCourseId;
+        int result = super.hashCode();
         result = 31 * result + (cookDatetime != null ? cookDatetime.hashCode() : 0);
         result = 31 * result + (weight != null ? weight.hashCode() : 0);
         result = 31 * result + (course != null ? course.hashCode() : 0);
@@ -83,7 +82,7 @@ public class CookedCourse {
     @Override
     public String toString() {
         return "CookedCourse{" +
-                "cookedCourseId=" + cookedCourseId +
+                "cookedCourseId=" + getCookedCourseId() +
                 ", cookDatetime=" + cookDatetime +
                 ", weight=" + weight +
                 ", course=" + course +
