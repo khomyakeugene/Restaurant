6985e1fdc21fa1db72997263453993c937db82d5
khomyakeugene <khomyak_eugene@yahoo.com>
2016-10-17 19:35:38 +0300

    Restaurant Project

--- src/main/java/com/company/restaurant/model/common/FloatLinkObject.java
+++ src/main/java/com/company/restaurant/model/common/FloatLinkObject.java
@@ -4,11 +4,11 @@ package com.company.restaurant.model.common;
  * Created by Yevhen on 23.05.2016.
  */
 public class FloatLinkObject extends LinkObject {
-    public Float getFloatLinkData() {
+    protected Float getFloatLinkData() {
         return (linkData == null) ? null : Float.parseFloat(linkData);
     }
 
-    public void setFloatLinkData(Float floatValue) {
+    protected void setFloatLinkData(Float floatValue) {
         this.linkData = (floatValue == null) ? null : Float.toString(floatValue);
     }
 }
